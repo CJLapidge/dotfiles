6bf1581e00a928addfebfd0782ef57abfa20279a
Christian Lapidge <christian.lapidge@gmail.com>
2020-11-02 19:05:53 +0000

    missing files from last commit

---
+++ .bash_profile
@@ -0,0 +1,2 @@
+alias gpom='git push origin master'
+
---
+++ install.sh
@@ -0,0 +1,5 @@
+#create symlinks
+ln -sv "~/dotfiles/.bash_profile" ~
+ln -sv "~/dotfiles/i3/config" ~/.config/i3/ #todo: test this
+ln -sv "~/dotfiles/.vimrc" ~
+ln -sv "~/dotfiles/.xinitrc" ~
